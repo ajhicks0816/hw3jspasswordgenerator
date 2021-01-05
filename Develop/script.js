99af8780edf6553129c62d48bd3d14e0121fa774
Alaina Hicks <ajhicks0816@gmail.com>
2021-01-05 17:24:26 -0600

    js code

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-generateBtn 
+
 
 // Write password to the #password input
 function writePassword() {
@@ -10,6 +10,26 @@ function writePassword() {
   passwordText.value = password;
 
 }
+  function getRandomLower() {
+    return(Math.floor(Math.random() * 26) + 97);
+  }
+
+  function getRandomUppder() {
+    return(Math.floor(Math.random() *26) + 65);
+  }
+
+  function getRandomNumber() {
+    return(Math.floor(Math.random() * 10) + 48)
+  }
+
+  function getRandomSymbol() {
+    const symbols = '!@#$%^&*(){}[]=<>/,.';
+    return symbols[Math.floor(Math.random() * symbols.length)];
+  }
+
+  console.log(getRandomSymbol());
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
