4601b42c0267d8cb2f2da4e354b702dd2cb9d8eb
Alaina Hicks <ajhicks0816@gmail.com>
2021-01-06 22:50:27 -0600

    generating information in generator box

--- Develop/script.js
+++ Develop/script.js
@@ -4,30 +4,29 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  // var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  var password = "abcdef"
 
   passwordText.value = password;
 
 }
 
-var newPassword = prompt("Do you need a new password?");
-var confirmPassword = confirm("Would you like lowercase letters?")
+// var newPassword = prompt("Do you need a new password?");
+// var confirmPassword = confirm("Would you like lowercase letters?")
 
-if (confirmPassword) {
-  
-}
+// if (confirmPassword) {
 
   function getRandomLower() {
-    return(Math.floor(Math.random() * 26) + 97);
+    return (Math.floor(Math.random() * 26) + 97);
   }
 
-  function getRandomUppder() {
-    return(Math.floor(Math.random() *26) + 65);
+  function getRandomUpper() {
+    return (Math.floor(Math.random() * 26) + 65);
   }
 
   function getRandomNumber() {
-    return(Math.floor(Math.random() * 10) + 48)
+    return (Math.floor(Math.random() * 10) + 48)
   }
 
   function getRandomSymbol() {
@@ -35,12 +34,11 @@ if (confirmPassword) {
     return symbols[Math.floor(Math.random() * symbols.length)];
   }
 
+  console.log(getRandomLower());
+  console.log(getRandomUpper());
+  console.log(getRandomNumber());
   console.log(getRandomSymbol());
 
 
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-function writePassword() {
-  alert ("writePassword");
-}
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
