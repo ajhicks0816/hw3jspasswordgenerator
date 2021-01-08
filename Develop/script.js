e3ee46493103e375596c42b9410485eef061cad5
Alaina Hicks <ajhicks0816@gmail.com>
2021-01-07 21:24:25 -0600

    testing out charset on all var

--- Develop/script.js
+++ Develop/script.js
@@ -1,78 +1,75 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-if (generateBtn) = 
 
 // Write password to the #password input
 function writePassword() {
-
-  //var password = generatePassword();
-  //var passwordText = document.querySelector("#password");
-  //var password = "abcdef"
-
-  //passwordText.value = password;
-
-// var newPassword = prompt("Do you need a new password?");
-
-var getRandomLowercase = confirm("Would you like lowercase letters?")
-if (getRandomLowercase) {
-  alert("ok, include random lowercase");
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
 }
-else {
-  alert("skip random lowercase")
-}
+  //function getRandomLowercase() {
+    //return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+  //}
 
-var getRandomUppercase = confirm("Would you like uppercase letters?");
-if (getRandomUppercase) {
-  alert("ok, include random uppercase");
-}
-else {
-  alert("skip random uppercase")
-}
+  //function getRandomUppercase() {
+    //return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+  //}
 
-var getRandomNumber = confirm("Would you like random numbers?")
-if (getRandomNumber) {
-  alert("ok, include random numbers");
-}
-else {
-  alert("skip random numbers")
-}
+  //function getRandomNumber() {
+    //return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+  //}
 
-var getRandomSymbol = confirm("Would you like random symbols?")
-if (getRandomSymbol) {
-  alert("ok, include random symbols");
-}
+  //function getRandomSymbol() {
+    //const symbols = '!@#$%^&*(){}[]=<>/,.';
+    //return symbols[Math.floor(Math.random() * symbols.length)];
+  //}
+  var passwordCharset = 0;
+  var newPassword = 0;
 
-else {
-  alert("skip random symbols")
-}
+  var generatePassword = prompt("Let's gets started")
+
+  var newPassword = prompt("Do you need a new password?");
+  if (newPassword) {
+    alert("let's get started");
+  }
+
+  var getRandomLowercase = confirm("Do you want lowercase letters?")
+  if (getRandomLowercase) {
+    passwordCharset += getRandomLowercase;
+    alert("ok, include random lowercase");
+  } else {
+    alert("skip random lowercase")
+  }
+
+  var getRandomUppercase = confirm("Do you want uppercase letters?");
+  if (getRandomUppercase) {
+    passwordCharset += getRandomUppercase;
+    alert("ok, include random uppercase");
+  } else {
+    alert("skip random uppercase")
+  }
+
+  var getRandomNumber = confirm("Do you want random numbers?")
+  if (getRandomNumber) {
+    passwordCharset += getRandomNumber;
+    alert("ok, include random numbers");
+  } else {
+    alert("skip random numbers")
+  }
+
+  var getRandomSymbol = confirm("Do you want random symbols?")
+  if (getRandomSymbol) {
+    passwordCharset += getRandomSymbol;
+    alert("ok, include random symbols");
+  } else {
+    alert("skip random symbols")
+  }
+
+  console.log(getRandomLowercase());
+  console.log(getRandomUppercase());
+  console.log(getRandomNumber());
+  console.log(getRandomSymbol());
 
 
-
-
-
-
-function getRandomLowercase() {
-  return (Math.floor(Math.random() * 26) + 97);
-}
-
-function getRandomUppercase() {
-  return (Math.floor(Math.random() * 26) + 65);
-}
-
-function getRandomNumber() {
-  return (Math.floor(Math.random() * 10) + 48)
-}
-
-function getRandomSymbol() {
-  const symbols = '!@#$%^&*(){}[]=<>/,.';
-  return symbols[Math.floor(Math.random() * symbols.length)];
-}
-
-console.log(getRandomLowercase());
-console.log(getRandomUppercase());
-console.log(getRandomNumber());
-console.log(getRandomSymbol());
-
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  //Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
