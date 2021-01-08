6f0471bc1baf2fe1e687af8781585c93e26e2c44
Alaina Hicks <ajhicks0816@gmail.com>
2021-01-07 22:31:48 -0600

    update to ReadMe, generator not working at all

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
-}
+//Write password to the #password input
+//function writePassword()
+//var password = generatePassword();
+//var passwordText = document.querySelector("#password");
+//passwordText.value = password;
+//}
+
 //function getRandomLowercase() {
 //return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 //}
@@ -23,49 +24,52 @@ function writePassword() {
 //const symbols = '!@#$%^&*(){}[]=<>/,.';
 //return symbols[Math.floor(Math.random() * symbols.length)];
 //}
-var passwordCharset = 0;
-var newPassword = 0;
 
-function generatePassword() {
-  var passwordLength = prompt("Your password needs to be a length of at least 8 characters but no more thatn 129 characters");
+//var passwordCharset = 0;
+//var newPassword = 0;
+
+function generatePassword()
+  var passwordLength = prompt("Your password must be between 8 and 128 characters");
   if (passwordLength < 8, passwordLength > 128) {
     alert("Your password did not meet the criteria, please do over")
   } else {
     return;
   }
 
-  var newPassword = prompt("Do you need a new password?");
-  if (newPassword) {
-    alert("let's get started");
-  }
+  //var newPassword = alert("Do you need a new password?");
+  //if (newPassword) {
+    //alert("let's get started");
+  //} else {
 
-  var getRandomLowercase = confirm("Do you want lowercase letters?")
+  //}
+
+  var getRandomLowercase = prompt("Do you want lowercase letters?")
   if (getRandomLowercase) {
-    passwordCharset += getRandomLowercase;
+    //passwordCharset += getRandomLowercase;
     alert("ok, include random lowercase");
   } else {
     alert("skip random lowercase")
   }
 
-  var getRandomUppercase = confirm("Do you want uppercase letters?");
+  var getRandomUppercase = prompt("Do you want uppercase letters?");
   if (getRandomUppercase) {
-    passwordCharset += getRandomUppercase;
+    //passwordCharset += getRandomUppercase;
     alert("ok, include random uppercase");
   } else {
     alert("skip random uppercase")
   }
 
-  var getRandomNumber = confirm("Do you want random numbers?")
+  var getRandomNumber = prompt("Do you want random numbers?")
   if (getRandomNumber) {
-    passwordCharset += getRandomNumber;
+    //passwordCharset += getRandomNumber;
     alert("ok, include random numbers");
   } else {
     alert("skip random numbers")
   }
 
-  var getRandomSymbol = confirm("Do you want random symbols?")
+  var getRandomSymbol = prompt("Do you want random symbols?")
   if (getRandomSymbol) {
-    passwordCharset += getRandomSymbol;
+    //passwordCharset += getRandomSymbol;
     alert("ok, include random symbols");
   } else {
     alert("skip random symbols")
@@ -79,4 +83,4 @@ function generatePassword() {
 
 
   //Add event listener to generate button
-  generateBtn.addEventListener("click", ();
+  generateBtn.addEventListener("click", generatePassword);
