6071f88c3cdca4997248a77b26a9351853c2fa6a
Alaina Hicks <ajhicks0816@gmail.com>
2021-01-07 21:50:57 -0600

    function for length of password

--- Develop/script.js
+++ Develop/script.js
@@ -7,26 +7,32 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
-  //function getRandomLowercase() {
-    //return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-  //}
+//function getRandomLowercase() {
+//return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+//}
 
-  //function getRandomUppercase() {
-    //return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-  //}
+//function getRandomUppercase() {
+//return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+//}
 
-  //function getRandomNumber() {
-    //return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
-  //}
+//function getRandomNumber() {
+//return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+//}
 
-  //function getRandomSymbol() {
-    //const symbols = '!@#$%^&*(){}[]=<>/,.';
-    //return symbols[Math.floor(Math.random() * symbols.length)];
-  //}
-  var passwordCharset = 0;
-  var newPassword = 0;
+//function getRandomSymbol() {
+//const symbols = '!@#$%^&*(){}[]=<>/,.';
+//return symbols[Math.floor(Math.random() * symbols.length)];
+//}
+var passwordCharset = 0;
+var newPassword = 0;
 
-  var generatePassword = prompt("Let's gets started")
+function generatePassword() {
+  var passwordLength = prompt("Your password needs to be a length of at least 8 characters but no more thatn 129 characters");
+  if (passwordLength < 8, passwordLength > 128) {
+    alert("Your password did not meet the criteria, please do over")
+  } else {
+    return;
+  }
 
   var newPassword = prompt("Do you need a new password?");
   if (newPassword) {
@@ -65,11 +71,12 @@ function writePassword() {
     alert("skip random symbols")
   }
 
-  console.log(getRandomLowercase());
-  console.log(getRandomUppercase());
-  console.log(getRandomNumber());
-  console.log(getRandomSymbol());
+  console.log(getRandomLowercase);
+  console.log(getRandomUppercase);
+  console.log(getRandomNumber);
+  console.log(getRandomSymbol);
+  console.log(passwordLength);
 
 
   //Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
+  generateBtn.addEventListener("click", ();
